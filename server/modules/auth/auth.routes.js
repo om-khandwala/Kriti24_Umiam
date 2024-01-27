072b0edd8da5073be61f6acdba645e22fca28689
pranjal <pv291003@gmail.com>
2024-01-27 16:54:08 +0530

    Login feature is ready

---
+++ server/modules/auth/auth.routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { login, redirect, logout } from './auth.controller.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+router.get('/login', login);
+router.get('/redirect', redirect);
+router.get('/logout', logout);
+
+export default router;
+
--- server/server.js
+++ server/server.js
@@ -1,31 +1,26 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRoutes from './modules/auth/auth.routes.js';
+import dotenv from "dotenv";
 
+const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(proceess.env.ATLAS_URI,{
-    useNewUrlParse: true,
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-mongoose.connection.once("open",()=>{
+mongoose.connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
-})
+});
 
-app.use("/api",require("./routes"));
+app.use('/', authRoutes);
 
-
-
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-   });
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
