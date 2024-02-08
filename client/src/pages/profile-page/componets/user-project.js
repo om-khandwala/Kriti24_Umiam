11d45d3bde35b400bb1b12e6c6003bec0642acde
ritu <rituraj1@gmail.com>
2024-02-08 23:01:36 +0530

    Fixed bugs in user project and profile  pages

--- client/src/pages/profile-page/componets/user-project.js
+++ client/src/pages/profile-page/componets/user-project.js
@@ -1,14 +1,27 @@
-import './component.css'
-import Project from './project';
+import { useEffect, useState } from "react";
+import "./component.css";
+import Project from "./project";
+import { userProjects } from "../../../api/project";
 
-function UserProject (){
-    return(
-        <div class='user-projects'>
-            <Project />
-            <Project />
-            <Project /> 
-        </div>
-    )  
+function UserProject({ userData }) {
+  const id = userData._id;
+  // console.log(id);
+  const [userProject, setUserProject] = useState([]);
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const data = await userProjects(id);
+      setUserProject(data);
+    };
+    fetchProjects();
+  }, [id]);
+  // console.log(userProject, "User projects are");
+  return (
+    <div class="user-projects">
+      {userProject.map((project) => {
+        return <Project project={project} />;
+      })}
+    </div>
+  );
 }
 
 export default UserProject;
