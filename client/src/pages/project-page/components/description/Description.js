e904424e6ed97d713523f8fb2d31e4a3f3839c78
MOHD_FAIZ <faaizmohd829@gmail.com>
2024-02-09 14:42:20 +0530

    project page is added

---
+++ client/src/pages/project-page/components/description/Description.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+function Description() {
+  return (
+    <div className='Describe'>
+      <p className='descp3'>Description</p>
+        <div className="boxing3">
+        </div>
+    </div>
+  )
+}
+
+export default Description
---
+++ client/src/pages/project-page/components/media/media.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+function Media() {
+  return (
+    <div className='media1'>
+      <p className='descp4'>Media</p>
+        <div className="boxing4">
+        </div>
+    </div>
+  )
+}
+
+export default Media
