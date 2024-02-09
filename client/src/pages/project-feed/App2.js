d8392b232a0969611e233477451b4e7ee49adfa8
ritu <rituraj1@gmail.com>
2024-02-09 17:57:30 +0530

    Added project-feed page

--- client/src/pages/project-feed/App2.js
+++ client/src/pages/project-feed/App2.js
@@ -1,19 +1,31 @@
-import Navbar from './components/Navbarfeed'
-import SearchProject from './components/Projectsearch'
-import Projectspace from './components/ProjectSpace'
-import Aboutproject from './components/ProjectsList'
+import SearchProject from "./components/Projectsearch";
+import Projectspace from "./components/ProjectSpace";
+import Aboutproject from "./components/ProjectsList";
+import "./index.css";
+import { useEffect, useState } from "react";
+import { allProject } from "../../api/project";
+import Navbar from "../../componets/navbar/navbar";
+function ProjectFeed({ user }) {
+  const [userProject, setUserProject] = useState([]);
+  useEffect(() => {
+    const fetchProject = async () => {
+      const data = await allProject();
+      setUserProject(data);
+    };
+    fetchProject();
+  }, []);
 
-function ProjectFeed() {
   return (
     <div className="App">
-        <Navbar />
-        <SearchProject />
-        <div className="view_project">
-          <Projectspace />
-          <div className="project_deatil">
-            <Aboutproject />
-          </div>
-        </div>
+      <Navbar />
+      <SearchProject />
+      <div className="view_project">
+        <Projectspace setUserProject={setUserProject} user={user} />
+        {userProject.length > 0 &&
+          userProject.map((project) => {
+            return <Aboutproject userProject={project} />;
+          })}
+      </div>
     </div>
   );
 }
--- client/src/pages/project-feed/components/ProjectsList.js
+++ client/src/pages/project-feed/components/ProjectsList.js
@@ -1,25 +1,34 @@
-import './ProjectsList.css'; 
+import "./ProjectsList.css";
 
-const Aboutproject = () => {
-    return ( 
-        <div className="about_proj">
-            <div className="proj_title">
-                <p className='title'>Weather App</p>
-            </div>
-            <div className="proj_img">
-                <img src='./images/Rectangle 67.png' alt='project related images' className='projrelimg'></img>
-                <img src='./images/Rectangle 67.png' alt='project related images' className='projrelimg'></img>
-            </div>
-            <div className="proj_details">
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-            </div>
-            <div className="tech_used">
-                <button className='tools'>HTML</button>
-                <button className='tools'>Javascript</button>
-                <button className='tools'>Mondo db</button>
-            </div>
-        </div>
-     );
-}
- 
+const Aboutproject = ({ userProject }) => {
+  console.log(userProject, "faaf");
+  return (
+    <div className="about_proj">
+      <div className="proj_title">
+        <p className="title">{userProject.ProjectName}</p>
+      </div>
+      <div className="proj_img">
+        {userProject.links.image.map((element) => {
+          return (
+            <img
+              src={element}
+              alt="project related images"
+              className="projrelimg"
+            ></img>
+          );
+        })}
+      </div>
+      <div className="proj_details">
+        <p>{userProject.description}</p>
+      </div>
+      <div className="tech_used">
+        {userProject.tags.map((tag) => {
+          console.log(tag);
+          return <button className="tools">{tag}</button>;
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default Aboutproject;
