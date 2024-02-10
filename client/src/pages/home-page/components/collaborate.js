8d35bcb6c18c0b50e2e243ec1f4ff4d9fb2c63a7
pranjal <pv291003@gmail.com>
2024-02-10 16:13:12 +0530

    fix course page css

--- client/src/pages/home-page/components/collaborate.js
+++ client/src/pages/home-page/components/collaborate.js
@@ -2,13 +2,12 @@ import './collaborate.css';
 
 const Collaborate = () => {
     const handlelogin = (e)=>{
-        window.location.href = "https://localhost:5050/login";
-      }
-
+        window.location.href = "https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c/oauth2/v2.0/authorize?client_id=3d6bc04c-160d-49a6-8ffd-d119b9f663d7&scope=user.read%20openid%20profile%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A5050%2Fredirect&client-request-id=fd1a356c-f763-48a2-be28-dd59cecd4972&response_mode=query&response_type=code&x-client-SKU=msal.js.node&x-client-VER=2.6.2&x-client-OS=darwin&x-client-CPU=arm64&client_info=1";
+    }
 
     return ( 
         <>
-        <div className="collaborate-hm ">
+        <div className="collaborate-hm">
             <div>
                 <p className="heading-hm">Collaberate And Discuss your project with campus janta</p>
                 <p className='col_text-hm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius diam justo, id sagittis ipsum dictum feugiat.</p>
